6ef6a629c6591dc093bade1a56c9e5ac9a26bc9e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-07-13 14:22:11 +0900

    power to switch-to

--- switch-to.cpp
+++ switch-to.cpp
@@ -3,9 +3,14 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    BOOL bFlag = TRUE;
+    if (argc < 2)
         return -1;
+    if (argc >= 3)
+    {
+        bFlag = atoi(argv[2]);
+    }
     HWND hwnd = (HWND)strtol(argv[1], NULL, 0);
-    SwitchToThisWindow(hwnd, TRUE);
+    SwitchToThisWindow(hwnd, bFlag);
     return 0;
 }
